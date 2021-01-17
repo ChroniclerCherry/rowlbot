28948d2fd3260606080f75f41856063af706c56f
Yiminxue Zheng <Yiminxuezheng@gmail.com>
2021-01-17 11:27:22 -0500

    actually you know what we don't need a database

--- bot.py
+++ bot.py
@@ -2,9 +2,10 @@ import os
 import discord
 from dotenv import load_dotenv
 import random
-import Database
+from discord.ext import commands
 
 load_dotenv()
+bot = commands.Bot(command_prefix='!')
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
@@ -20,5 +21,15 @@ async def on_message(message):
 
     if random.random() < 0.00001:
         await message.channel.send("AAAAAAAAAAAAAAAAAAAAA")
+
+@bot.command()
+async def command(ctx, action, command_name, postID):
+    if (action == "add"):
+        query = "SELECT * FROM commands"
+        with Database as db:
+            result = db(query)
+            result.fetchall()
+    elif(action == "remove"):
+        query = "SELECT * FROM commands"
 
 client.run(TOKEN)
