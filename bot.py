79c2d230470ca13ebae212214490fcebc7a323cc
Yiminxue Zheng <Yiminxuezheng@gmail.com>
2021-01-16 22:04:20 -0500

    Added db

--- bot.py
+++ bot.py
@@ -1,15 +1,22 @@
 import os
-
 import discord
 from dotenv import load_dotenv
+import random
+import Database
 
-load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if random.random() < 0.00001:
+        await message.channel.send("AAAAAAAAAAAAAAAAAAAAA")
 
 client.run(TOKEN)
